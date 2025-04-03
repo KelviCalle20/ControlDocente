ee544f5580139d21cda92b4c6d8816a852427068
zatan <zatan@DESKTOP-4M3P6EF>
2025-04-02 20:25:06 -0400

    integrantes del proyecto

--- src/main/java/com/mycompany/controldocentes/ControlDocentes.java
+++ src/main/java/com/mycompany/controldocentes/ControlDocentes.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.controldocentes;
 
 /**
- *
- * @author zatan
+ *integrantes:  Kelvin Calle
+ *              Denilson Apaza
  */
 import java.awt.*;
 import java.sql.*;
